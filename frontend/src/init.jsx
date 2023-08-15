711bece0b42faec14e7f18b657e8d7ec88acc65e
Ildar Sahautdinov <scams1992@gmail.com>
2023-08-15 21:29:16 +0500

    disable rollbar

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -1,7 +1,7 @@
 import { Provider } from 'react-redux';
 import i18next from 'i18next';
 import { I18nextProvider, initReactI18next } from 'react-i18next';
-import { Provider as ProviderRoll, ErrorBoundary } from '@rollbar/react';
+// import { Provider as ProviderRoll, ErrorBoundary } from '@rollbar/react';
 // import Rollbar from "rollbar";
 import React from 'react';
 import filter from 'leo-profanity';
@@ -58,31 +58,28 @@ const init = async (socket) => {
     );
   });
 
-  const rollbarConfig = {
-    accessToken: process.env.REACT_APP_ROLLBAR_TOKEN,
-    captureUncaught: true,
-    captureUnhandledRejections: true,
-    environment: 'production',
-  };
+  // const rollbarConfig = {
+  //   accessToken: process.env.REACT_APP_ROLLBAR_TOKEN,
+  //   captureUncaught: true,
+  //   captureUnhandledRejections: true,
+  //   environment: 'production',
+  // };
 
   // const rollbar = new Rollbar(rollbarConfig);
   //config={rollbarConfig} v Proveder
-  //<ErrorBoundary>
+  // <ProviderRoll config={rollbarConfig}>
+  // <ErrorBoundary>
   return (
     <React.StrictMode>
-      <ProviderRoll config={rollbarConfig}>
-        <ErrorBoundary>
-          <Provider store={store}>
-            <WSocketProvider socket={socket}>
-              <AuthProvider>
-                <I18nextProvider i18n={i18n}>
-                  <App />
-                </I18nextProvider>
-              </AuthProvider>
-            </WSocketProvider>
-          </Provider>
-        </ErrorBoundary>
-      </ProviderRoll>
+      <Provider store={store}>
+        <WSocketProvider socket={socket}>
+          <AuthProvider>
+            <I18nextProvider i18n={i18n}>
+              <App />
+            </I18nextProvider>
+          </AuthProvider>
+        </WSocketProvider>
+      </Provider>
     </React.StrictMode>
   );
 };
