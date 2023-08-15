da865b5cafb80c7e599e8dca67dcf52751ba360b
Ildar Sahautdinov <scams1992@gmail.com>
2023-08-15 09:48:06 +0500

    add check current user

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -3,13 +3,14 @@ import React, { createContext, useState } from "react";
 export const AuthContext = createContext({});
 
 const AuthProvider = ({ children }) => {
-  const [userName, setUserName] = useState(null);
+  const currentUser = JSON.parse(localStorage.getItem("userId"));
+  const [userName, setUserName] = useState(
+    currentUser ? { username: currentUser.username } : null
+  );
   const logIn = (name) => setUserName(name);
   const logOut = () => {
-    const a = null;
-    return a.hello();
-    // localStorage.removeItem("userId");
-    // setUserName(null);
+    localStorage.removeItem("userId");
+    setUserName(null);
   };
 
   return (
