de096da96dbee92460db3dfe6c8a7fc638d7e5a2
Ildar Sahautdinov <scams1992@gmail.com>
2023-09-06 11:18:34 +0500

    fix network problem form

--- frontend/src/components/chatPage/MainPage.jsx
+++ frontend/src/components/chatPage/MainPage.jsx
@@ -3,18 +3,18 @@ import { useSelector, useDispatch } from 'react-redux';
 import { unwrapResult } from '@reduxjs/toolkit';
 import { toast } from 'react-toastify';
 import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
 
 import { getChannels } from '../../slices/channelsSlice.js';
 import { loadingSelector, setStatus } from '../../slices/loadingSlice.js';
 import { getAuthHeader, useAuth } from '../../context/AuthContext.jsx';
 import SpinnerForm from './components/SpinnerForm.jsx';
-import UndefinedErrorForm from './components/undefinedErrorForm.jsx';
 import MainForm from './components/MainForm.jsx';
+import errorImage from '../../assets/unknown.svg';
 
 const uiState = {
   loading: SpinnerForm,
   success: MainForm,
-  failed: UndefinedErrorForm,
 };
 
 const ChatPage = () => {
@@ -31,9 +31,11 @@ const ChatPage = () => {
         if (error.status === 401) {
           return auth.logOut();
         } if (error.status === 0) {
+          console.log('tyt__+');
           toast.warn(t('toast.networkProblem'));
           return dispatch(setStatus('undefinedError'));
         }
+        console.log('ne tut');
         return dispatch(setStatus('undefinedError'));
       });
   }, [auth.userName, dispatch, auth, t]);
@@ -42,10 +44,27 @@ const ChatPage = () => {
     return null;
   }
 
+  const handleRefresh = () => window.location.reload();
   const Component = uiState[loadingStatus];
 
   return (
-    <Component />
+    Component ? <Component /> : (
+      <div className="text-center">
+        <img
+          alt={t('dataLoadFail')}
+          className="img-fluid h-25"
+          src={errorImage}
+        />
+        <h1 className="h4 text-muted">{t('dataLoadFail')}</h1>
+        <p className="text-muted">
+          {t('refreshMsg1')}
+          {' '}
+          <Link to="/" onClick={handleRefresh}>
+            {t('refreshMsg2')}
+          </Link>
+        </p>
+      </div>
+    )
   );
 };
 
